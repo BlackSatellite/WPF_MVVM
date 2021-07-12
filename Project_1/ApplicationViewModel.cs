c76e3be549ae802d65deef412d5aadc8153dacee
John <wesatellite@bigmir.net>
2021-07-12 22:28:01 +0300

    Initial commit

---
+++ Project_1/ApplicationViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Project_1
+{
+    // Модель представления
+    public class ApplicationViewModel: INotifyPropertyChanged
+    {
+        private Phone _selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        public Phone SelectedPhone 
+        {
+            get { return _selectedPhone; }
+            set 
+            {
+                _selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel() 
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="iPhone 7", Company="Apple", Price=56000 },
+                new Phone {Title="Galaxy S7 Edge", Company="Samsung", Price =60000 },
+                new Phone {Title="Elite x3", Company="HP", Price=56000 },
+                new Phone {Title="Mi5S", Company="Xiaomi", Price=35000 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "") 
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ Project_1/Phone.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Project_1
+{
+    // Класс Phone, который будет представлять модель приложения.
+    public class Phone: INotifyPropertyChanged
+    {
+        private string _title;
+        private string _company;
+        private int _price;
+
+        public string Title 
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public string Company 
+        {
+            get { return _company; }
+            set 
+            {
+                _company = value;
+                OnPropertyChanged("Company");
+            }
+        }
+
+        public int Price 
+        {
+            get { return _price; }
+            set 
+            {
+                _price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "") 
+        {
+            if ( PropertyChanged != null ) 
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
